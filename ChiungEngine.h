52b190e8f11ed947c45c13018654d775f7e40201
chiung22 <chiung4137@gmail.com>
2026-05-18 01:54:52 +0900

    1000판 알고리즘 치웅 파일

--- ChiungEngine.h
+++ ChiungEngine.h
@@ -7,12 +7,14 @@
 #include <random>
 #include <thread>
 #include <atomic>
+#include <mutex>
 #include <cmath>
 #include <iomanip>
 
+// [에러 수정] 윈도우 환경에서 64비트 비트 카운팅 함수 호환성 및 int 캐스팅 형변환 완벽 처리
 #ifdef _MSC_VER
 #include <intrin.h>
-#define __builtin_popcountll __popcnt64
+#define __builtin_popcountll(x) (int)__popcnt64(x)
 inline int __builtin_ctzll(uint64_t mask) { unsigned long index; _BitScanForward64(&index, mask); return (int)index; }
 #endif
 
@@ -25,18 +27,19 @@ namespace Chiung {
     const int PLAYER1 = 1;
     const int PLAYER2 = 2;
 
-    static uint64_t ADJACENT_MASK[49];
-    static uint64_t JUMP_MASK[49];
-    static uint64_t ZOBRIST_TABLE[49][2];
-    static uint64_t ZOBRIST_TURN;
+    inline static uint64_t ADJACENT_MASK[49];
+    inline static uint64_t JUMP_MASK[49];
+    inline static uint64_t ZOBRIST_TABLE[49][2];
+    inline static uint64_t ZOBRIST_TURN;
 
     enum TTFlag { EXACT, LOWERBOUND, UPPERBOUND };
+
     struct TTEntry { uint64_t hash; int depth; int flag; int value; };
     const int TT_SIZE = 1048576;
-    static vector<TTEntry> TT_Cache(TT_SIZE);
+    inline static vector<TTEntry> TT_Cache(TT_SIZE);
 
-    static atomic<bool> searchCancelled(false);
-    static atomic<bool> ponderCancelled(false);
+    inline static atomic<bool> searchCancelled{ false };
+    inline static atomic<bool> ponderCancelled{ false };
 
     struct Move {
         int from, to; bool isClone; int infectCount; int orderScore;
@@ -67,21 +70,14 @@ namespace Chiung {
 
     class AtaxxEngine {
     public:
-        static int nodeCounter;
-        static time_point<steady_clock> searchStartTime;
-        static double timeLimitSeconds;
-        static int reachedDepth;
+        // [구문 오류 수정] 소괄호 초기화 문법을 중괄호 초기화 형태로 변경하여 함수 선언 오인(Most Vexing Parse) 차단
+        inline static atomic<long long> totalGameNodes{ 0 };
+        inline static atomic<long long> ttHits{ 0 };
+        inline static atomic<long long> ttLookups{ 0 };
 
-        static long long ttHits;
-        static long long ttLookups;
-        static long long totalGameNodes;
-
-        static void checkTime() {
-            if (searchCancelled || ponderCancelled) return;
-            if (duration<double>(steady_clock::now() - searchStartTime).count() >= timeLimitSeconds) {
-                searchCancelled = true;
-            }
-        }
+        inline static Move bestMoveOverall;
+        inline static int highestReachedDepth = 0;
+        inline static mutex bestMoveMutex;
 
         static int evaluate(const Bitboard& b, int myColor) {
             uint64_t my = (myColor == PLAYER1) ? b.p1 : b.p2;
@@ -95,34 +91,33 @@ namespace Chiung {
 
             double myPercent = ((double)myCount / total) * 100.0;
             int pScore = myCount - oppCount;
-            int mScore = 0, dScore = 0;
+            int mobilityScore = 0, densityScore = 0;
 
             uint64_t temp = my;
             while (temp) {
                 int idx = __builtin_ctzll(temp);
-                dScore += __builtin_popcountll(ADJACENT_MASK[idx] & my);
-                mScore += __builtin_popcountll((ADJACENT_MASK[idx] | JUMP_MASK[idx]) & empty);
+                densityScore += __builtin_popcountll(ADJACENT_MASK[idx] & my);
+                mobilityScore += __builtin_popcountll((ADJACENT_MASK[idx] | JUMP_MASK[idx]) & empty);
                 temp &= temp - 1;
             }
 
-            if (myPercent < 45.0) return pScore * 10 + dScore * 60;
-            else if (myPercent <= 55.0) return pScore * 20 + mScore * 80;
+            if (myPercent < 45.0) return pScore * 10 + densityScore * 60;
+            else if (myPercent <= 55.0) return pScore * 20 + mobilityScore * 80;
             else return pScore * 100;
         }
 
         static int minimax(const Bitboard& b, int depth, int alpha, int beta, bool isMax, int myColor) {
-            if ((++nodeCounter & 2047) == 0) checkTime();
-            if (searchCancelled || ponderCancelled) return 0;
+            if (searchCancelled) return 0;
 
-            totalGameNodes++;
+            totalGameNodes.fetch_add(1, memory_order_relaxed);
 
             int alphaOrig = alpha;
-            int ttIndex = b.hashKey & (TT_SIZE - 1);
+            int ttIndex = (int)(b.hashKey & (TT_SIZE - 1));
             TTEntry& tte = TT_Cache[ttIndex];
 
-            ttLookups++;
+            ttLookups.fetch_add(1, memory_order_relaxed);
             if (tte.hash == b.hashKey && tte.depth >= depth) {
-                ttHits++;
+                ttHits.fetch_add(1, memory_order_relaxed);
                 if (tte.flag == EXACT) return tte.value;
                 if (tte.flag == LOWERBOUND && tte.value > alpha) alpha = tte.value;
                 if (tte.flag == UPPERBOUND && tte.value < beta) beta = tte.value;
@@ -141,14 +136,14 @@ namespace Chiung {
                 Bitboard nextB = applyMove(b, m, currColor);
                 int val = minimax(nextB, depth - 1, alpha, beta, !isMax, myColor);
 
-                if (searchCancelled || ponderCancelled) return 0;
+                if (searchCancelled) return 0;
 
                 if (isMax) { best = max(best, val); alpha = max(alpha, best); }
                 else { best = min(best, val); beta = min(beta, best); }
                 if (beta <= alpha) break;
             }
 
-            if (!searchCancelled && !ponderCancelled) {
+            if (!searchCancelled) {
                 tte.hash = b.hashKey; tte.depth = depth; tte.value = best;
                 if (best <= alphaOrig) tte.flag = UPPERBOUND;
                 else if (best >= beta) tte.flag = LOWERBOUND;
@@ -212,41 +207,128 @@ namespace Chiung {
             return b;
         }
 
-        // [추가됨] 시뮬레이션용 지정 깊이(Depth) 탐색 함수
+        // [식별자 오류 수정] main.cpp 교차 검증에 필수적인 고정 깊이 탐색(Fixed Depth) 함수 완전체 구현 복구
         static Move getBestMoveFixedDepth(const Bitboard& b, int myColor, int targetDepth) {
             searchCancelled = false;
             ponderCancelled = false;
-            nodeCounter = 0;
+            totalGameNodes = 0;
             ttHits = 0;
             ttLookups = 0;
-            totalGameNodes = 0;
 
             vector<Move> validMoves = getValidMoves(b, myColor);
             if (validMoves.empty()) return { -1, -1, false, 0, 0 };
 
-            Move bestMove = validMoves[0];
-            int maxEval = -1000000, alpha = -1000000, beta = 1000000;
+            Move globalBestMove = validMoves[0];
 
-            for (const Move& m : validMoves) {
-                Bitboard nextB = applyMove(b, m, myColor);
-                int eval = minimax(nextB, targetDepth - 1, alpha, beta, false, myColor);
-                if (eval > maxEval) { maxEval = eval; bestMove = m; }
-                alpha = max(alpha, eval);
+            // 반복 심화 결합으로 초기 알파-베타 컷오프 효율성 최적화
+            for (int currentDepth = 1; currentDepth <= targetDepth; currentDepth++) {
+                Move depthBestMove = validMoves[0];
+                int maxEval = -1000000, alpha = -1000000, beta = 1000000;
+
+                auto it = find(validMoves.begin(), validMoves.end(), globalBestMove);
+                if (it != validMoves.end() && it != validMoves.begin()) {
+                    iter_swap(validMoves.begin(), it);
+                }
+
+                for (const Move& m : validMoves) {
+                    Bitboard nextB = applyMove(b, m, myColor);
+                    int eval = minimax(nextB, currentDepth - 1, alpha, beta, false, myColor);
+                    if (eval > maxEval) { maxEval = eval; depthBestMove = m; }
+                    alpha = max(alpha, eval);
+                }
+                globalBestMove = depthBestMove;
             }
-            return bestMove;
+            return globalBestMove;
+        }
+
+        static Move getBestMoveTimeLimited(const Bitboard& b, int myColor, double timeLimitSeconds) {
+            searchCancelled = false;
+            totalGameNodes = 0;
+            ttHits = 0;
+            ttLookups = 0;
+            highestReachedDepth = 0;
+
+            vector<Move> validMoves = getValidMoves(b, myColor);
+            if (validMoves.empty()) return { -1, -1, false, 0, 0 };
+            if (validMoves.size() == 1) return validMoves[0];
+
+            bestMoveOverall = validMoves[0];
+
+            int numThreads = (int)std::thread::hardware_concurrency();
+            if (numThreads == 0) numThreads = 4;
+
+            vector<thread> workers;
+
+            for (int t = 0; t < numThreads; t++) {
+                workers.emplace_back([&, t, validMoves, myColor, b]() {
+                    vector<Move> threadMoves = validMoves;
+
+                    if (t > 0) {
+                        mt19937 rng(12345 + t);
+                        shuffle(threadMoves.begin() + 1, threadMoves.end(), rng);
+                    }
+
+                    Move localBestMove = threadMoves[0];
+
+                    for (int depth = 1; depth <= 100; depth++) {
+                        int maxEval = -1000000, alpha = -1000000, beta = 1000000;
+                        Move depthBestMove = threadMoves[0];
+
+                        auto it = find(threadMoves.begin(), threadMoves.end(), localBestMove);
+                        if (it != threadMoves.end() && it != threadMoves.begin()) {
+                            iter_swap(threadMoves.begin(), it);
+                        }
+
+                        for (const Move& m : threadMoves) {
+                            Bitboard nextB = applyMove(b, m, myColor);
+                            int eval = minimax(nextB, depth - 1, alpha, beta, false, myColor);
+                            if (searchCancelled) break;
+
+                            if (eval > maxEval) { maxEval = eval; depthBestMove = m; }
+                            alpha = max(alpha, eval);
+                        }
+
+                        if (searchCancelled) break;
+                        localBestMove = depthBestMove;
+
+                        lock_guard<mutex> lock(bestMoveMutex);
+                        if (depth > highestReachedDepth || (t == 0 && depth == highestReachedDepth)) {
+                            highestReachedDepth = depth;
+                            bestMoveOverall = localBestMove;
+                        }
+                    }
+                    });
+            }
+
+            auto start = high_resolution_clock::now();
+            while (duration<double>(high_resolution_clock::now() - start).count() < timeLimitSeconds) {
+                this_thread::sleep_for(milliseconds(5));
+            }
+
+            searchCancelled = true;
+
+            for (auto& w : workers) {
+                if (w.joinable()) w.join();
+            }
+
+            return bestMoveOverall;
         }
 
         static void initMasks() {
             mt19937_64 rng(12345);
             for (int i = 0; i < 49; i++) {
+                ADJACENT_MASK[i] = 0; JUMP_MASK[i] = 0;
                 ZOBRIST_TABLE[i][0] = rng(); ZOBRIST_TABLE[i][1] = rng();
-                for (int r = -2; r <= 2; r++) {
-                    for (int c = -2; c <= 2; c++) {
-                        int nr = (i / 7) + r, nc = (i % 7) + c;
+                int r = i / 7; int c = i % 7;
+                for (int dr = -2; dr <= 2; dr++) {
+                    for (int dc = -2; dc <= 2; dc++) {
+                        if (dr == 0 && dc == 0) continue;
+                        int nr = r + dr; int nc = c + dc;
                         if (nr >= 0 && nr < 7 && nc >= 0 && nc < 7) {
                             int nidx = nr * 7 + nc;
-                            if (abs(r) <= 1 && abs(c) <= 1) ADJACENT_MASK[i] |= (1ULL << nidx);
-                            else JUMP_MASK[i] |= (1ULL << nidx);
+                            int dist = max(abs(dr), abs(dc));
+                            if (dist == 1) ADJACENT_MASK[i] |= (1ULL << nidx);
+                            else if (dist == 2) JUMP_MASK[i] |= (1ULL << nidx);
                         }
                     }
                 }
@@ -254,13 +336,5 @@ namespace Chiung {
             ZOBRIST_TURN = rng();
         }
     };
-
-    int AtaxxEngine::nodeCounter = 0;
-    int AtaxxEngine::reachedDepth = 0;
-    double AtaxxEngine::timeLimitSeconds = 3.0;
-    time_point<steady_clock> AtaxxEngine::searchStartTime;
-    long long AtaxxEngine::ttHits = 0;
-    long long AtaxxEngine::ttLookups = 0;
-    long long AtaxxEngine::totalGameNodes = 0;
 
 } // end namespace Chiung
