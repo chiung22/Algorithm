a1b5ce92c1c6214810d184a26de7b83ef0d48b0d
chiung22 <chiung4137@gmail.com>
2026-06-22 01:24:29 +0900

    고착화 해결

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-#include "ChiungEngine.h" // 폰더링이 탑재된 최종 알파 엔진
+#include "ChiungEngine.h" // 폰더링 탑재 최신 엔진
 
 using namespace std;
 
@@ -18,39 +18,47 @@ int main() {
     // 파이썬 프로그램이 명령을 내릴 때까지 무한 대기
     while (cin >> cmd) {
         if (cmd == "INIT") {
-            // 시간 입력 받던 부분을 제거하고 내 색상만 입력받음
             cin >> myColor;
+            // [추가] 재시작 버튼을 누르면 엔진의 보드판도 초기 상태로 리셋!
+            board.init();
             cout << "READY" << endl;
         }
         else if (cmd == "OPP") {
             int fr, fc, tr, tc;
             cin >> fr >> fc >> tr >> tc;
-            int from = fr * 7 + fc;
-            int to = tr * 7 + tc;
-            int dist = max(abs(fr - tr), abs(fc - tc));
-            Chiung::Move m = { from, to, (dist == 1), 0, 0 };
-            board = Chiung::AtaxxEngine::applyMove(board, m, (myColor == 1 ? 2 : 1));
+
+            // [추가] 상대방이 둘 곳이 없어서 패스(Pass)한 경우
+            if (fr == -1 && fc == -1) {
+                board.hashKey ^= Chiung::ZOBRIST_TURN; // 턴만 넘김
+            }
+            else {
+                int from = fr * 7 + fc;
+                int to = tr * 7 + tc;
+                int dist = max(abs(fr - tr), abs(fc - tc));
+                Chiung::Move m = { from, to, (dist == 1), 0, 0 };
+                board = Chiung::AtaxxEngine::applyMove(board, m, (myColor == 1 ? 2 : 1));
+            }
             cout << "ACK" << endl;
         }
         else if (cmd == "THINK") {
-            // [자동 연산 시간 분배 로직] (초반: 3초, 중반: 5초, 후반 끝내기: 7초)
             int emptyCount = 49 - (Chiung::popcount64(board.p1) + Chiung::popcount64(board.p2));
-            double currentLimit = 3.0; // 기본 초반 탐색 시간
+            double currentLimit = 3.0; // 기본 3초
 
-            if (emptyCount <= 35 && emptyCount > 15) {
-                currentLimit = 5.0; // 중반부 (복잡도 증가)
-            }
-            else if (emptyCount <= 15) {
-                currentLimit = 7.0; // 후반부 (엔드게임 확정 계산)
-            }
+            if (emptyCount <= 35 && emptyCount > 15) currentLimit = 5.0;
+            else if (emptyCount <= 15) currentLimit = 7.0;
 
-            // AI 최적 수 탐색
             Chiung::Move best = Chiung::AtaxxEngine::getBestMoveTimeLimited(board, myColor, currentLimit);
-            board = Chiung::AtaxxEngine::applyMove(board, best, myColor);
 
-            // 파이썬에게 결과 전송
-            cout << "BESTMOVE " << (best.from / 7) << " " << (best.from % 7) << " "
-                << (best.to / 7) << " " << (best.to % 7) << " " << (best.isClone ? 1 : 0) << endl;
+            // [추가] AI 본인도 둘 곳이 없어서 패스(Pass)해야 하는 경우
+            if (best.from == -1) {
+                board.hashKey ^= Chiung::ZOBRIST_TURN;
+                cout << "BESTMOVE -1 -1 -1 -1 0" << endl;
+            }
+            else {
+                board = Chiung::AtaxxEngine::applyMove(board, best, myColor);
+                cout << "BESTMOVE " << (best.from / 7) << " " << (best.from % 7) << " "
+                    << (best.to / 7) << " " << (best.to % 7) << " " << (best.isClone ? 1 : 0) << endl;
+            }
         }
     }
     return 0;
